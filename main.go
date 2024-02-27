732533b54e1c1921df584642b4bb0797a622cfdd
Felix Ingram <felix.ingram@nccgroup.com>
2024-02-27 15:27:31 +0000

    Pre tutorial save

--- main.go
+++ main.go
@@ -9,12 +9,14 @@ import (
 
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/cloudwatchlogs"
+	"github.com/aws/aws-sdk-go-v2/service/sts"
 	"github.com/charmbracelet/bubbles/list"
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
 )
 
 var (
+	errorStyle        = lipgloss.NewStyle().Foreground(lipgloss.Color("9"))
 	titleStyle        = lipgloss.NewStyle().MarginLeft(2)
 	itemStyle         = lipgloss.NewStyle().PaddingLeft(4)
 	selectedItemStyle = lipgloss.NewStyle().PaddingLeft(2).Foreground(lipgloss.Color("170"))
@@ -28,7 +30,7 @@ const listHeight = 15
 type itemDelegate struct{}
 type item string
 
-func (i item) FilterValue() string { return "" }
+func (i item) FilterValue() string { return string(i) }
 
 func (d itemDelegate) Height() int                             { return 1 }
 func (d itemDelegate) Spacing() int                            { return 0 }
@@ -52,66 +54,80 @@ func (d itemDelegate) Render(w io.Writer, m list.Model, index int, listItem list
 }
 
 type model struct {
-	cursor    int
-	logGroups list.Model
-	// viewport  viewport.Model
-	ready bool
+	logGroups []string
+	// cursor    int
+	// logGroups list.Model
+	// ready     bool
 }
 
-func initialModel() model {
-	defaultWidth := 20
-	l := list.New([]list.Item{}, itemDelegate{}, defaultWidth, listHeight)
-	l.Title = "What do you want for dinner?"
-	l.SetShowStatusBar(false)
-	l.SetFilteringEnabled(true)
-	l.Styles.Title = titleStyle
-	l.Styles.PaginationStyle = paginationStyle
-	l.Styles.HelpStyle = helpStyle
-
-	return model{cursor: 0, logGroups: l}
-}
+// func initialModel() model {
+// 	defaultWidth := 20
+// 	l := list.New([]list.Item{}, itemDelegate{}, defaultWidth, listHeight)
+// 	l.Title = "What do you want for dinner?"
+// 	l.SetShowStatusBar(false)
+// 	l.SetFilteringEnabled(true)
+// 	l.Styles.Title = titleStyle
+// 	l.Styles.PaginationStyle = paginationStyle
+// 	l.Styles.HelpStyle = helpStyle
+//
+// 	return model{cursor: 0, logGroups: l}
+// }
 
 func (m model) Init() tea.Cmd {
 	return nil
 }
 
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-	var (
-		cmd tea.Cmd
-	)
-
-	switch msg := msg.(type) {
-	case tea.KeyMsg:
-		if msg.String() == "q" {
-			return m, tea.Quit
-		}
-	case tea.WindowSizeMsg:
-		m.logGroups.SetWidth(msg.Width)
-		return m, nil
-	}
-	m.logGroups, cmd = m.logGroups.Update(msg)
-
-	return m, cmd
-
+	//  	var (
+	//  		cmd tea.Cmd
+	//  	)
+	//
+	//  	switch msg := msg.(type) {
+	//  	case tea.KeyMsg:
+	//  		if m.logGroups.FilterState() == list.Filtering {
+	//  			break
+	//  		}
+	//  		if msg.String() == "q" {
+		// 			return m, tea.Quit
+		// 		}
+		// 	case tea.WindowSizeMsg:
+		// 		m.logGroups.SetWidth(msg.Width)
+		// 		m.logGroups.SetHeight(msg.Height)
+		// 		return m, nil
+		// 	}
+		// 	m.logGroups, cmd = m.logGroups.Update(msg)
+		//
+		// 	return m, cmd
+		//
+	return m, nil
 }
 
 func (m model) View() string {
-	return m.logGroups.View()
+	return strings.Join(m.logGroups, "\n")
+	// return m.logGroups.View()
 }
 
 func main() {
 
-	model := initialModel()
-	config, err := config.LoadDefaultConfig(context.TODO(), config.WithRegion("eu-west-2"))
+	model := model{}
+	config, err := config.LoadDefaultConfig(context.TODO(), config.WithRegion("eu-west-1"))
 	if err != nil {
 		log.Fatalf("failed to load configuration, %v", err)
+	}
+
+	sts_client := sts.NewFromConfig(config)
+
+	_, err = sts_client.GetCallerIdentity(context.Background(), &sts.GetCallerIdentityInput{})
+	if err != nil {
+		fmt.Println(errorStyle.Render(fmt.Sprintf("Bad AWS Credentials: %v ", err)))
+		return
 	}
 
 	client := cloudwatchlogs.NewFromConfig(config)
 
 	paginator := cloudwatchlogs.NewDescribeLogGroupsPaginator(client, &cloudwatchlogs.DescribeLogGroupsInput{})
 
-	groups := []list.Item{}
+	// groups := []list.Item{}
 
 	for paginator.HasMorePages() {
 		output, err := paginator.NextPage(context.Background())
@@ -119,13 +135,14 @@ func main() {
 			log.Fatalf("failed to list log groups, %v", err)
 		}
 		for _, logGroup := range output.LogGroups {
-			groups = append(groups, item(string(*logGroup.LogGroupName)))
+			model.logGroups = append(model.logGroups, string(*logGroup.LogGroupName))
+			// groups = append(groups, item(string(*logGroup.LogGroupName)))
 		}
 	}
 
-	model.logGroups.SetItems(groups)
+	// model.logGroups.SetItems(groups)
 
-	btProg := tea.NewProgram(model, tea.WithAltScreen())
+	btProg := tea.NewProgram(model)
 
 	if _, err := btProg.Run(); err != nil {
 		log.Fatalf("failed to start program, %v", err)
